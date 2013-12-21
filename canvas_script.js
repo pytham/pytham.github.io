515ece66edf9fe86b7e02ce88e5bae72825c8569
pytham <gouthamraju.k@gmail.com>
2013-12-21 10:51:24 +0530

    Update canvas_script.js

--- canvas_script.js
+++ canvas_script.js
@@ -2,23 +2,24 @@ function OnCanvasClick(){
 	var myCanvas = document.getElementById('canvas');
 	var context = myCanvas.getContext("2d");
 	
-	$( "#canvas" ).mousedown(function(evt){
-		var offset = $( "#canvas" ).offset();
+		$( '#canvas' ).mousedown(function(evt){
 		
+		var offset = $( '#canvas' ).offset();
 		context.beginPath();
-		context.moveTo(evt.pageX - offset.left , evt.pageY - offset.top );
+		context.moveTo(evt.pageX - offset.left, evt.pageY - offset.top );
 		
 		$( document ).mousemove(function(evt){
-			var offset = $( "#canvas" ).offset();
-			
-			context.lineTo(evt.pageX - offset.left , evt.pageY - offset.top );
+			var offset = $( '#canvas' ).offset();
+			context.lineTo(evt.pageX - offset.left, evt.pageY - offset.top);
 			context.lineWidth = 1;
-			context.strokeStyle = red;
+			context.strokeStyle = 'blue';
 			context.stroke();
-		}).mouseup(function(){
+		});
+		$( document ).mouseup(function()
+				{
 					$( document ).unbind('mousemove');
 					$( document ).unbind('mouseup');
-		});
+				});
 	});
 	
 }
