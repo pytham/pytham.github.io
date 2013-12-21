3af5f8f9b3256229ed12f0b489ff0e263881a124
pytham <gouthamraju.k@gmail.com>
2013-12-21 10:34:48 +0530

    small correction

--- canvas_script.js
+++ canvas_script.js
@@ -2,14 +2,14 @@ function OnCanvasClick(){
 	var myCanvas = document.getElementById('canvas');
 	var context = myCanvas.getContext("2d");
 	
-	$("#canvas").mousedown(function(evt){
-		var offset = $("#canvas").offset();
+	$( "#canvas" ).mousedown(function(evt){
+		var offset = $( "#canvas" ).offset();
 		
 		context.beginPath();
 		context.moveTo(evt.pageX - offset.left , evt.pageY - offset.top );
 		
-		$(document).mousemove(function(evt){
-			var offset = $("#canvas").offset();
+		$( document ).mousemove(function(evt){
+			var offset = $( "#canvas" ).offset();
 			
 			context.lineTo(evt.pageX - offset.left , evt.pageY - offset.top );
 			context.lineWidth = 1;
