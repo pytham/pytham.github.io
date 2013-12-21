bf14e20823d1988d4149d6a39d36a8556857ce01
pytham <gouthamraju.k@gmail.com>
2013-12-21 11:39:29 +0530

    correction

--- canvas_script.js
+++ canvas_script.js
@@ -2,35 +2,23 @@ function OnCanvasClick(){
 	var myCanvas = document.getElementById('canvas');
 	var context = myCanvas.getContext("2d");
 	
-<<<<<<< HEAD
-	$( "#canvas" ).mousedown(function(evt){
-		var offset = $( "#canvas" ).offset();
-=======
-		$( '#canvas' ).mousedown(function(evt){
->>>>>>> 515ece66edf9fe86b7e02ce88e5bae72825c8569
+	myCanvas.onmousedown(function(evt){
+		var offset = myCanvas.offset();
 		
-		var offset = $( '#canvas' ).offset();
 		context.beginPath();
-		context.moveTo(evt.pageX - offset.left, evt.pageY - offset.top );
+		context.moveTo(evt.pageX - offset.left , evt.pageY - offset.top );
 		
-		$( document ).mousemove(function(evt){
-<<<<<<< HEAD
-			var offset = $( "#canvas" ).offset();
+		document.onmousemove(function(evt){
+			var offset = myCanvas.offset();
 			
 			context.lineTo(evt.pageX - offset.left , evt.pageY - offset.top );
-=======
-			var offset = $( '#canvas' ).offset();
-			context.lineTo(evt.pageX - offset.left, evt.pageY - offset.top);
->>>>>>> 515ece66edf9fe86b7e02ce88e5bae72825c8569
 			context.lineWidth = 1;
-			context.strokeStyle = 'blue';
+			context.strokeStyle = red;
 			context.stroke();
+		}).onmouseup(function(){
+					document.unbind('mousemove');
+					document.unbind('mouseup');
 		});
-		$( document ).mouseup(function()
-				{
-					$( document ).unbind('mousemove');
-					$( document ).unbind('mouseup');
-				});
 	});
 	
 }
