2e9bc459d77a8c00397aedc963e7a74fbc1f1af1
pytham <gouthamraju.k@gmail.com>
2013-12-24 14:34:59 +0530

    Update canvas_script.js

--- canvas_script.js
+++ canvas_script.js
@@ -8,7 +8,6 @@ function onStartWrite(){
 		$( document ).mousemove(function(evt){
 			context.lineTo(evt.pageX - myCanvas.offsetLeft, evt.pageY - myCanvas.offsetTop);
 			context.stroke();
-			$( '#canvas' ).offset({ top: evt.pageY, left: evt.pageX})
 		}).mouseup(function(evt){
 			$( document ).unbind('mousemove');
 			$( document ).unbind('mouseup');
