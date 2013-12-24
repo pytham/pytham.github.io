760a16515530577291c35f1d5ff396a00bccbbc5
pytham <gouthamraju.k@gmail.com>
2013-12-24 07:27:37 +0530

    Update canvas_script.js

--- canvas_script.js
+++ canvas_script.js
@@ -1,4 +1,4 @@
-function OnCanvasClick(evt){
+function OnStartWrite(evt){
 	var myCanvas = document.getElementById('canvas');
 	var context = myCanvas.getContext("2d");
 	
@@ -6,12 +6,20 @@ function OnCanvasClick(evt){
 		context.moveTo(evt.pageX - myCanvas.offsetLeft, evt.pageY - myCanvas.offsetTop);
 		
 		$( document ).mousemove(function(evt){
-			//context.lineTo(evt.pageX - myCanvas.offsetLeft, evt.pageY - myCanvas.offsetTop);
-			//context.stroke();
+			context.lineTo(evt.pageX - myCanvas.offsetLeft, evt.pageY - myCanvas.offsetTop);
+			context.stroke();
 			$( '#canvas' ).offset({ top: evt.pageY, left: evt.pageX})
 		}).mouseup(function(evt){
 			$( document ).unbind('mousemove');
 			$( document ).unbind('mouseup');
 
 	});
+}
+
+function moveCanvas(evt){
+	$( document ).mousemove(function(evt){
+			$( '#canvas' ).offset({ top: evt.pageY, left: evt.pageX})
+		}).mousedown(function(evt){
+			$( document ).unbind('mousemove');
+			$( document ).unbind('mousedown');
 }
