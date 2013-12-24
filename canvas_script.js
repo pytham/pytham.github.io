beceddeb56dd70a7b7f152a86b84c92c0246f4a9
pytham <gouthamraju.k@gmail.com>
2013-12-25 00:24:22 +0530

    Update canvas_script.js

--- canvas_script.js
+++ canvas_script.js
@@ -19,11 +19,17 @@ document.getElementById('canvas').addEventListener('mousedown',function(evt){
 
 function moveCanvas(){
 	$( document ).mousemove(function(evt){
-			document.style.cursor=move;
+			document.style.cursor='move';
 			$( '#canvas' ).offset({ top: evt.pageY, left: evt.pageX})
 		}).mousedown(function(evt){
 			$( document ).unbind('mousemove');
 			$( document ).unbind('mousedown');
-			document.style.cursor=default;
+			document.style.cursor='default';
 		});	
+}
+
+function clearCanvas(){
+	var myCanvas = document.getElementById('canvas');
+	var context = myCanvas.getContext("2d");
+	context.fillRect('200','200','')
 }
